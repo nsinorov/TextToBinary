1d22ac50f589134ce3df86781931a1ed46c117f9
Nikolay Sinorov <nsinorov123@gmail.com>
2023-01-08 00:38:20 +0200

    Add files via upload

---
+++ Converter/Converter/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+string input = Console.ReadLine();
+
+byte[] data = System.Text.Encoding.ASCII.GetBytes(input);
+
+foreach (byte b in data)
+{
+    Console.Write(Convert.ToString(b, 2).PadLeft(8, '0') + " ");
+}
+Console.WriteLine();
